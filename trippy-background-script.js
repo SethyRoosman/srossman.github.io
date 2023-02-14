ddb706767c77a94fab6d0bcb1295e370a19c9b68
Seth R <sar1074@unh.edu>
2023-02-14 11:14:04 -0500

    trying a new more artsy/complex design

--- trippy-background-script.js
+++ trippy-background-script.js
@@ -1,1 +1,3 @@
-trippy-background-script.js
+/* this script will basically go through a list of picture/screensaver
+ where the camera is zoomed in 65() percent and the, possibly
+  add acceleration ad guassian blur */
